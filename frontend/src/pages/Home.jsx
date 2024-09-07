c6171632284f261bccceb915a2b2a654855042ae
ShayanIsrar <shayanisrar19@gmail.com>
2024-09-07 10:12:24 -0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
     const fetchProducts = async ()=>{
         try {
-            const url = 'http://localhost:8080/products';
+            const url = 'mern-signup-form-psi.vercel.app/products';
             const headers = {
                 headers:{
                     'Authorization': localStorage.getItem('token')
