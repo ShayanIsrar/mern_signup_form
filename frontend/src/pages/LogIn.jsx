6341e7e9ca39de8276cd30f72c99009130e58d5d
ShayanIsrar <shayanisrar19@gmail.com>
2024-09-07 10:16:03 -0700

    Update LogIn.jsx

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -25,7 +25,7 @@ const LogIn = () => {
         }
 
         try {
-            const url = 'http://localhost:8080/auth/login';
+            const url = 'mern-signup-form-psi.vercel.app/auth/login';
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
