574e5a79655a1e07273d41f65ce458909b420e49
ShayanIsrar <shayanisrar19@gmail.com>
2024-09-07 22:50:53 -0700

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
         }
 
         try {
-            const url = 'https://mern-signup-form-psi.vercel.app/auth/signup';
+            const url = 'https://mern-signup-form-zofm.vercel.app/auth/signup';
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
